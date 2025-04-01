7a4eba7ce3313ccf5c9b7ba27acdf6c72f30d2c9
Fredrik Edenqvist <fredrik.edenqvist@b3.se>
2025-04-01 12:33:42 +0200

    file move

--- Toll/Calculator/Vehicle.cs
+++ Toll/Calculator/Vehicle.cs
@@ -1,24 +1,16 @@
 ﻿namespace TollFeeCalculator;
 
+/// <summary>
+/// TollFreeVehicles:
+/// Motorbike
+/// Tractor
+/// Emergency
+/// Diplomat
+/// Foreign
+/// Military
+/// </summary>
 public interface Vehicle
 {
     string GetVehicleType();
     bool PaysTollFee { get; }
 }
-
-public static class VehicleExtensions
-{
-    public static bool IsTollFreeVehicle(this Vehicle? vehicle)
-    {
-        return !(vehicle?.PaysTollFee ?? true);
-    }
-}
-
-// Side note:
-// TollFreeVehicles:
-// Motorbike
-// Tractor
-// Emergency
-// Diplomat
-// Foreign
-// Military
---
+++ Toll/Calculator/VehicleExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace TollFeeCalculator;
+
+public static class VehicleExtensions
+{
+    public static bool IsTollFreeVehicle(this Vehicle? vehicle)
+    {
+        return !(vehicle?.PaysTollFee ?? true);
+    }
+}
